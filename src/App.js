1221e712eed42455d849cf011b149cd085123ec7
Israt05 <107882724+Israt05@users.noreply.github.com>
2023-01-30 14:54:10 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          Learn statics Hosting with AWS
+          Learn statics by Israt with AWS
       </header>
     </div>
   );
